4758a3146ea1e18cde4a0e5d56be81d3c68d77f4
Walter909 <walter.toriola@gmail.com>
2023-04-23 19:41:49 -0700

    FrontPage in progress

---
+++ src/Components/SideButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+import Game from '../Icons/Game.js';
+
+const SideButton = () => {
+  return <Button startIcon={Game}>Button</Button>;
+};
+
+export default SideButton;
